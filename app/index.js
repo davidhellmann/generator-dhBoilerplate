5f6e826a8dd657148d5d38fdefe50e2cb2429bea
David Hellmann <davidhellmann.com@gmail.com>
2015-10-23 12:29:14 +0200

    Update

--- app/index.js
+++ app/index.js
@@ -1,105 +1,109 @@
 'use strict';
-var util    = require('util');
-var path    = require('path');
-var yeoman  = require('yeoman-generator');
-var yosay   = require('yosay');
-var chalk   = require('chalk');
+var util   = require('util');
+var path   = require('path');
+var yeoman = require('yeoman-generator');
+var yosay  = require('yosay');
+var chalk  = require('chalk');
 
 var dhBoilerplateGenerator = yeoman.generators.Base.extend({
 
-    init: function() {
-        this.pkg = require('../package.json');
-    },
+  init: function () {
+    this.pkg = require('../package.json');
+  },
 
-    askFor: function() {
-        var done = this.async();
+  askFor: function () {
+    var done = this.async();
 
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the impressive ' + chalk.red('dhBoilerplate') + ' generator!'
-        ));
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the impressive ' + chalk.red('dhBoilerplate') + ' generator!'
+    ));
 
-        var warning =
-            '\n This generator is customized for my own needs!'
-            + '\n So it may not be the right thing for you!'
-            + '\n Keep this in mind when you go further and please read the Readme'
-            + '\n'
-            + '\n Also this is very early version and maybe there are some bugs :)'
-            + '\n';
+    var welcome =
+      '\n '+
+      '\n      _  _      ___              _                        _           _   '+
+      '\n     ( )( )    (  _`\         _ (_ )                     (_ )        ( )_'+
+      '\n    _| || |__  | (_) )   _   (_) | |    __   _ __  _ _    | |    _ _ | ,_)  '+
+      '\n  /'_` ||  _ `\|  _ <' /'_`\ | | | |  /'__`\( '__)( '_`\  | |  /'_` )| |   /'__`\'+
+      '\n ( (_| || | | || (_) )( (_) )| | | | (  ___/| |   | (_) ) | | ( (_| || |_ (  ___/'+
+      '\n `\__,_)(_) (_)(____/'`\___/'(_)(___)`\____)(_)   | ,__/'(___)`\__,_)`\__)`\____)'+
+      '\n                                                  | | '+
+      '\n                                                  (_) '+
+      '\n ';
 
-        console.log(chalk.bold.red(warning));
+    console.log(welcome);
 
-        var prompts = [
-            {
-                type: 'input',
-                name: 'projectName',
-                message: 'Name the project (name of the theme folder in Wordpress)',
-                default: 'dhBoilerplate'
-            },{
-                type: 'input',
-                name: 'projectDescription',
-                message: 'Short description of the Project`',
-                default: 'undefined'
-            },{
-                type: 'input',
-                name: 'projectProxy',
-                message: 'Enter the vhost for your Project'
-            }, {
-                type: 'list',
-                name: 'projectUsage',
-                message: 'Which purpose does this Project have? Choose the appropriate option',
-                choices: [
-                    "Just Prototyping",
-                    "Use with WordPress",
-                    "Use with Craft CMS"
-                ]
-            },{
-                type: 'input',
-                name: 'projectVersion',
-                message: 'Project Version Number',
-                default: '0.0.1'
-            },{
-                type: 'input',
-                name: 'projectAuthor',
-                message: 'Project Author or company',
-                default: 'undefined'
-            },{
-                type: 'input',
-                name: 'projectMail',
-                message: 'Mailadress of the author',
-                default: 'undefined'
-            },{
-                type: 'input',
-                name: 'projectUrl',
-                message: 'Author URl',
-                default: 'http://...'
-            },{
-                type: 'input',
-                name: 'projectRepo',
-                message: 'Git Repo URL',
-                default: 'http://...'
-            }
-        ];
+    var prompts = [
+      {
+        type:    'input',
+        name:    'projectName',
+        message: 'Name the project (name of the theme folder in Wordpress)',
+        default: 'dhBoilerplate'
+      }, {
+        type:    'input',
+        name:    'projectDescription',
+        message: 'Short description of the Project`',
+        default: 'undefined'
+      }, {
+        type:    'input',
+        name:    'projectProxy',
+        message: 'Enter the vhost for your Project'
+      }, {
+        type:    'list',
+        name:    'projectUsage',
+        message: 'Which purpose does this Project have? Choose the appropriate option',
+        choices: [
+          "Just Prototyping",
+          "Use with WordPress",
+          "Use with Craft CMS"
+        ]
+      }, {
+        type:    'input',
+        name:    'projectVersion',
+        message: 'Project Version Number',
+        default: '0.0.1'
+      }, {
+        type:    'input',
+        name:    'projectAuthor',
+        message: 'Project Author or company',
+        default: 'undefined'
+      }, {
+        type:    'input',
+        name:    'projectMail',
+        message: 'Mailadress of the author',
+        default: 'undefined'
+      }, {
+        type:    'input',
+        name:    'projectUrl',
+        message: 'Author URl',
+        default: 'http://...'
+      }, {
+        type:    'input',
+        name:    'projectRepo',
+        message: 'Git Repo URL',
+        default: 'http://...'
+      }
+    ];
 
-        this.prompt(prompts, function(props) {
-            this.projectName = props.projectName;
-            this.projectDescription = props.projectDescription;
-            this.projectProxy = props.projectProxy;
-            this.projectUsage = props.projectUsage;
-            this.projectVersion = props.projectVersion;
-            this.projectAuthor = props.projectAuthor;
-            this.projectMail = props.projectMail;
-            this.projectUrl = props.projectUrl;
-            this.projectRepo = props.projectRepo;
-            done();
-        }.bind(this));
+    this.prompt(prompts, function (props) {
+      this.projectName        = props.projectName;
+      this.projectDescription = props.projectDescription;
+      this.projectProxy       = props.projectProxy;
+      this.projectUsage       = props.projectUsage;
+      this.projectVersion     = props.projectVersion;
+      this.projectAuthor      = props.projectAuthor;
+      this.projectMail        = props.projectMail;
+      this.projectUrl         = props.projectUrl;
+      this.projectRepo        = props.projectRepo;
+      done();
+    }.bind(this));
 
-    },
+  },
 
-    app: function() {
-        // move src folder
-        this.directory('templates/', 'templates/');
-    },
+  app: function () {
+    // move src folder
+    this.directory('*/**', 'templates/');
+  },
 
 });
 
