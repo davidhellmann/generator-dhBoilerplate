58b541136506994466a57f72cc8b6b786d7ee826
David Hellmann <davidhellmann.com@gmail.com>
2015-12-18 15:46:38 +0100

    Add some bower stuff as default

---
+++ app/templates/___src/assets/js/scripts/lazyloading.js
@@ -0,0 +1,30 @@
+/**
+ * Lazy Loading Stuff
+ */
+
+/*
+* Lazy Sizes
+* */
+
+;(function($) {
+
+
+
+})();
+
+
+
+/*
+* Blazy
+* */
+
+;(function($) {
+
+  /*var bLazy = new Blazy({
+    offset: 500,
+    // success: function(ele){
+    //   console.log('image loaded.');
+    // }
+  });*/
+
+})(jQuery);
---
+++ app/templates/___src/assets/js/scripts/waypoints.js
@@ -0,0 +1,36 @@
+/**
+ * Waypoints
+ */
+
+;(function($) {
+
+  /*  var $_tabs = $('#products');
+
+   $_tabs.waypoint(function(direction) {
+   if (direction === 'down') {
+   $(this.element).find('.m_sliderTabs').addClass('sticky');
+   }
+   }, {
+   offset: '50'
+   });
+
+   $_tabs.waypoint(function(direction) {
+   if (direction === 'up') {
+   $(this.element).find('.m_sliderTabs').removeClass('sticky');
+   }
+   }, {
+   offset: '0%'
+   });
+
+   $('#map_wrapper').waypoint(function(direction) {
+   if (direction === 'down') {
+   $('.m_sliderTabs').addClass("beamMeUp");
+   setTimeout(function(){
+   $('.m_sliderTabs').removeClass("beamMeUp sticky");
+   }, 500);
+   }
+   }, {
+   offset: '140'
+   });*/
+
+}(jQuery));
