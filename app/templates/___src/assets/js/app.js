0ab81fec73ae6b40e30cd38357befa3c55baa7f4
David Hellmann <davidhellmann.com@gmail.com>
2016-03-01 19:48:54 +0100

    Updte app.js

--- app/templates/___src/assets/js/app.js
+++ app/templates/___src/assets/js/app.js
@@ -2,33 +2,33 @@
  * Plugins
  * */
 
-/*var $               = require('jquery');
- var eq              = require('css-element-queries');
- var gsap            = require('gsap');
- var matchheight     = require('jquery-match-height');
- var jscookie        = require('js-cookie');
- var lazysizes       = require('lazysizes');
- var magnificpopup   = require('magnific-popup');
- var pace            = require('pace-progress');
- var photoswipe      = require('photoswipe');
- var photoswipeUI    = require('../../../node_modules/photoswipe/dist/photoswipe-ui-default');
- var picturefill     = require('picturefill');
- var skrollr         = require('skrollr');
- var slick           = require('slick-carousel');
- var svginjector     = require('svg-injector');
- //var wp            = require('waypoints/lib/jquery.waypoints');
+/*import $               from 'jquery'
+import cq              from 'cq-prolyfill'
+import gsap            from 'gsap'
+import matchheight     from 'jquery-match-height'
+import jscookie        from 'js-cookie'
+import lazysizes       from 'lazysizes'
+import magnificpopup   from 'magnific-popup'
+import pace            from 'pace-progress'
+import photoswipe      from 'photoswipe'
+import photoswipeUI    from '../../../node_modules/photoswipe/dist/photoswipe-ui-default'
+import picturefill     from 'picturefill'
+import skrollr         from 'skrollr'
+import slick           from 'slick-carousel'
+import svginjector     from 'svg-injector'
+//import wp            from 'waypoints/lib/jquery.waypoints'
 
 
- /!*
- * Scripts
- * *!/
+/!*
+* Scripts
+* *!/
 
- var bodyclass       = require('./scripts/bodyclass');
- var accordion       = require('./scripts/accordion');
- var example         = require('./scripts/example');
- var layer           = require('./scripts/layer');
- var lazyloading     = require('./scripts/lazyloading');
- var pace            = require('./scripts/pace');
- var scrollto        = require('./scripts/scrollto');
- var slick           = require('./scripts/slick');
- //var waypoints       = require('./scripts/waypoints');*/
+import bodyclass       from './scripts/bodyclass'
+import accordion       from './scripts/accordion'
+import example         from './scripts/example'
+import layer           from './scripts/layer'
+import lazyloading     from './scripts/lazyloading'
+import pace            from './scripts/pace'
+import scrollto        from './scripts/scrollto'
+import slick           from './scripts/slick'
+//import waypoints       from './scripts/waypoints'*!/*/
