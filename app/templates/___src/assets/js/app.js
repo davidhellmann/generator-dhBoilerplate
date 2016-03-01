4ef35ed57ea62fc9d34cea5444edeeea25621ffe
David Hellmann <davidhellmann.com@gmail.com>
2016-03-01 19:45:52 +0100

    Update app.js

--- app/templates/___src/assets/js/app.js
+++ app/templates/___src/assets/js/app.js
@@ -1,23 +1,34 @@
-var debug = true;
+/*
+ * Plugins
+ * */
 
-// Site Settings
-var defaults = {
-  breakpoints: {
-    start: 0,
-    small: 667,
-    medium: 1024,
-    large: 1280
-  },
-  timings: {
-    zero: 0,
-    fast: 250,
-    medium: 500,
-    slow: 1000,
-  }
-};
+/*var $               = require('jquery');
+ var eq              = require('css-element-queries');
+ var gsap            = require('gsap');
+ var matchheight     = require('jquery-match-height');
+ var jscookie        = require('js-cookie');
+ var lazysizes       = require('lazysizes');
+ var magnificpopup   = require('magnific-popup');
+ var pace            = require('pace-progress');
+ var photoswipe      = require('photoswipe');
+ var photoswipeUI    = require('../../../node_modules/photoswipe/dist/photoswipe-ui-default');
+ var picturefill     = require('picturefill');
+ var skrollr         = require('skrollr');
+ var slick           = require('slick-carousel');
+ var svginjector     = require('svg-injector');
+ //var wp            = require('waypoints/lib/jquery.waypoints');
 
-var _dh_options   = jQuery.extend(defaults, _dh_options);
 
-/**
- * Test
- */
+ /!*
+ * Scripts
+ * *!/
+
+ var bodyclass       = require('./scripts/bodyclass');
+ var accordion       = require('./scripts/accordion');
+ var example         = require('./scripts/example');
+ var layer           = require('./scripts/layer');
+ var lazyloading     = require('./scripts/lazyloading');
+ var pace            = require('./scripts/pace');
+ var scrollto        = require('./scripts/scrollto');
+ var slick           = require('./scripts/slick');
+ //var waypoints       = require('./scripts/waypoints');*/
