57107279ce5c6d60ff235c1c28bfc3c23e76b9f3
David Hellmann <davidhellmann.com@gmail.com>
2016-03-01 23:37:16 +0100

    Add browserSync Stuff

--- app/templates/___src/templates/wordpress/footer.php
+++ app/templates/___src/templates/wordpress/footer.php
@@ -7,15 +7,15 @@
   <!-- Javascripts -->
   <script src="<?php bloginfo('template_url'); ?>/assets/js/app.min.js?ver=<?php echo filemtime(get_stylesheet_directory() . '/assets/js/app.min.js'); ?>"></script>
 
-<?php
-$host = $_SERVER['HTTP_HOST'];
-if($host == "localhost" or $host == "yourwebsite.dev") {
-  ?>
+  <% if (projectUrl != 'http://...') { %>
   <!-- BrowserSync -->
   <script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.10.0.js'><\/script>".replace("HOST", location.hostname));
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.11.11.js'><\/script>".replace
+    ("HOST", location.hostname));
     //]]></script>
-<?php } ?>
+  <% } %>
 
 </body>
 </html>
+
+
