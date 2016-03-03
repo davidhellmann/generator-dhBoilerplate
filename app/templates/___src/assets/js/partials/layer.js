2cb7bcab45601a55aa3ab521346ade50163f3c61
David Hellmann <davidhellmann.com@gmail.com>
2016-03-03 17:39:38 +0100

    Updaet Layerjs

--- app/templates/___src/assets/js/partials/layer.js
+++ app/templates/___src/assets/js/partials/layer.js
@@ -1,53 +1,44 @@
-/**
+/*
  * Layer
- */
+ * */
 
-var _trigger = $('[data-js="fullLayer"]');
+// Open Layer Function
+const openLayer = (layer) => {
+  // Body
+  var _body = $('body');
 
-_trigger.on('click', function(e) {
+  _body.attr('data-status', 'layer-open');
+  $('.fullLayer[data-layer="'+layer+'"]').fadeIn(500);
+};
+
+
+// Close Layer Function
+const closeLayer = (layer) => {
+  // Body
+  var _body = $('body');
+
+  _body.attr('data-status', '');
+  $('.fullLayer[data-layer]').fadeOut(250);
+};
+
+
+// Open Layer Trigger
+var openTrigger = $('[data-js="fullLayer"]');
+openTrigger.on('click', function(e) {
   e.preventDefault();
 
   // Layer
   var _layer = $(this).attr('data-layer');
-
   openLayer(_layer);
 });
 
 
-
-/**
- * Close Layer
- */
-
-var _trigger = $('[data-js="fullLayer__close"]');
-
-_trigger.on('click', function(e) {
+// Close Layer Trigger
+var closeTrigger = $('[data-js="fullLayer__close"]');
+closeTrigger.on('click', function(e) {
   e.preventDefault();
 
   closeLayer();
 });
 
 
-
-/**
- * Functions
- */
-
-
-// Open Layer
-function openLayer(layer) {
-  // Body
-  var _body = $('body');
-
-  _body.attr('data-status', 'layer-open');
-  $('.fullLayer[data-layer="'+layer+'"]').fadeIn(500);
-}
-
-// Close Layer
-function closeLayer() {
-  // Body
-  var _body = $('body');
-
-  _body.attr('data-status', '');
-  $('.fullLayer[data-layer]').fadeOut(250);
-}
