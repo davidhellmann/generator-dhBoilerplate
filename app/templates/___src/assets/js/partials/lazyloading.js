f9ddfdbd308c66fabff84a7d57a9c2015457b9f6
David Hellmann <davidhellmann.com@gmail.com>
2016-03-03 20:09:20 +0100

    Add Lazysize Settings

--- app/templates/___src/assets/js/partials/lazyloading.js
+++ app/templates/___src/assets/js/partials/lazyloading.js
@@ -1,3 +1,7 @@
 /**
  * Lazy Loading Stuff
  */
+
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.expand = 130; //default 360-500
+lazySizesConfig.expFactor = 1.3; //default: 1.7
