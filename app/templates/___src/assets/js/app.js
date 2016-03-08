20f23a050ac9eec15622f16b30f494ce5f0da2d6
David Hellmann <davidhellmann.com@gmail.com>
2016-03-08 13:20:12 +0100

    remove picturefill

--- app/templates/___src/assets/js/app.js
+++ app/templates/___src/assets/js/app.js
@@ -3,42 +3,40 @@
 * */
 
 // jQuery
-import $               from 'jquery'
+import $                    from 'jquery'
 
 // Container Queries
-import cq              from 'cq-prolyfill'
+import cq                   from 'cq-prolyfill'
 
 // GSAP
-//import gsap            from 'gsap'
+//import gsap                 from 'gsap'
 
 // MatchHeight
-//import matchheight     from 'jquery-match-height'
+//import matchheight          from 'jquery-match-height'
 
 // jQuery Cookie
-//import jscookie        from 'js-cookie'
+//import jscookie             from 'js-cookie'
 
 // Lazy Sizes
-import lazysizes       from 'lazysizes'
-import lazysizesBGSet  from '../../../node_modules/lazysizes/plugins/bgset/ls.bgset.js'
+import lazysizes            from 'lazysizes'
+import lazysizesBGSet       from '../../../node_modules/lazysizes/plugins/bgset/ls.bgset.js'
+import lazysizesRespImg     from '../../../node_modules/lazysizes/plugins/respimg/ls.respimg.js'
 
 // Magnific Popup
-//import magnificpopup   from 'magnific-popup'
+//import magnificpopup        from 'magnific-popup'
 
 // Pace Progress
-//import paceprogress    from 'pace-progress'
+//import paceprogress         from 'pace-progress'
 
 // Photoswipe
-//import photoswipe      from 'photoswipe'
-//import photoswipeUI    from '../../../node_modules/photoswipe/dist/photoswipe-ui-default'
-
-// Picturefill
-import picturefill     from 'picturefill'
+//import photoswipe           from 'photoswipe'
+//import photoswipeUI         from '../../../node_modules/photoswipe/dist/photoswipe-ui-default'
 
 // Slick Carousel
-import slickcarousel   from 'slick-carousel'
+import slickcarousel        from 'slick-carousel'
 
 // SVG Injector
-//import svginjector     from 'svg-injector'
+//import svginjector          from 'svg-injector'
 
 
 /*
@@ -46,28 +44,28 @@ import slickcarousel   from 'slick-carousel'
  */
 
 // Accordion
-//import accordion          from './partials/accordion'
+//import accordion            from './partials/accordion'
 
 // BodyClass
-import bodyclass          from './partials/bodyclass'
+import bodyclass            from './partials/bodyclass'
 
 // Debounce
-import debounce           from './partials/debounce'
+import debounce             from './partials/debounce'
 
 // Example
-import example            from './partials/example'
+import example              from './partials/example'
 
 // Layer
-//import layer              from './partials/layer'
+//import layer                from './partials/layer'
 
 // Lazy Loading
-import lazyloading        from './partials/lazyloading'
+import lazyloading          from './partials/lazyloading'
 
 // Pace
-//import pace               from './partials/pace'
+//import pace                 from './partials/pace'
 
 // ScrollTo
-import scrollto           from './partials/scrollto'
+import scrollto             from './partials/scrollto'
 
 // Slick
-import slick              from './partials/slick'
+import slick                from './partials/slick'
