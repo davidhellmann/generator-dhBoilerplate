ac385fde03ab69cbfcf42262861f80cdcecd463e
David Hellmann <davidhellmann.com@gmail.com>
2016-08-11 17:11:19 +0200

    Update Template Task

--- app/templates/___src/gulp/tasks/templates.js
+++ app/templates/___src/gulp/tasks/templates.js
@@ -8,31 +8,21 @@ const $ = gulpLoadPlugins()
 const templates = () => {
 
   <% if (projectUsage == 'Prototyping' ) { %>
-  return gulp
-    .src(config.src.templates+ '**/*.{html,twig,rss}')
-    .pipe($.plumber())
-    .pipe($.twig())
-    .on('error', errorHandler)
-    //.pipe($.changed(config.dist.markup))
-    .pipe(gulp.dest(config.dist.markup));
-  <% } %>
 
+      return gulp
+        .src(config.src.templates + '**/[^_]*.{html,twig,rss}')
+        .pipe($.plumber())
+        .pipe($.twig())
+        .on('error', errorHandler)
+        //.pipe($.changed(config.dist.markup))
+        .pipe(gulp.dest(config.dist.markup));
 
+  <% } else { %>
 
-  <% if (projectUsage == 'CraftCMS') { %>
-  return gulp
-    .src(config.src.templates + '**/*.{html,twig,rss}')
-    //.pipe($.changed(config.dist.markup))
-    .pipe(gulp.dest(config.dist.markup));
-  <% } %>
+      return gulp
+        .src(config.src.templates + '**/*.*')
+        .pipe(gulp.dest(config.dist.markup));
 
-
-
-  <% if (projectUsage == 'WordPress') { %>
-    return gulp
-      .src(config.src.templates + '**/*.{html,php,twig,png,css,md,rss}')
-      //.pipe($.changed(config.dist.markup))
-      .pipe(gulp.dest(config.dist.markup));
   <% } %>
 
 }
