acc92a47347eb08d82b3b0a3025c229a374524c3
David Hellmann <davidhellmann.com@gmail.com>
2016-09-03 21:47:32 +0200

    Update Path

--- app/templates/___src/templates/wordpress/_partials/_header/_meta.php
+++ app/templates/___src/templates/wordpress/_partials/_header/_meta.php
@@ -7,11 +7,11 @@
 <meta name="twitter:url" value="<?php bloginfo('home' ); ?>" />
 <meta name="twitter:title" value="<?php wp_title(); ?>" />
 <meta name="twitter:description" value="<?php wp_title(); ?>" />
-<meta name="twitter:image" value="<?php bloginfo('template_directory' ); ?>/assets/images/YOURIMAGE.jpg" />
+<meta name="twitter:image" value="<?php bloginfo('template_directory' ); ?>/assets/images/bitmap/YOURIMAGE.jpg" />
 <meta name="twitter:site" value="@davidhellmann" />
 <meta name="twitter:creator" value="@davidhellmann" />
 
 <meta property="og:title" content="<?php wp_title(); ?>"/>
-<meta property="og:image" content="<?php bloginfo('template_directory' ); ?>/assets/images/YOURIMAGE.jpg"/>
+<meta property="og:image" content="<?php bloginfo('template_directory' ); ?>/assets/images/bitmap/YOURIMAGE.jpg"/>
 <meta property="og:site_name" content="<?php wp_title(); ?>"/>
 <meta property="og:description" content="<?php wp_title(); ?>"/>
