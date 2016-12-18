c2e37dcb04ddc9396a930030009c6ae56aeddc6c
David Hellmann <davidhellmann.com@gmail.com>
2016-12-18 21:00:25 +0100

    Add Critical CSS

---
+++ app/templates/___src/gulp/tasks/crititcalcss.js
@@ -0,0 +1,19 @@
+import config from '../../config.json'
+import gulp from 'gulp'
+import critical from 'critical'
+
+
+const criticalcss = () => {
+  return critical.generate({
+    base: config.dist.markup,
+    src: '<%= proxyUrl %>',
+    dest: 'critical.min.css.html',
+    minify: true,
+    width: 1440,
+    height: 900,
+    ignore: ['font-face'],
+  })
+}
+
+gulp.task('criticalcss', criticalcss);
+module.exports = {criticalcss}
