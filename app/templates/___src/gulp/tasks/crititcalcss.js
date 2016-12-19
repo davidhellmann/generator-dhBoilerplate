c10c3159c87997770da5e55331899725460184de
David Hellmann <davidhellmann.com@gmail.com>
2016-12-19 17:30:50 +0100

    Update Task CCSS

--- app/templates/___src/gulp/tasks/crititcalcss.js
+++ app/templates/___src/gulp/tasks/crititcalcss.js
@@ -1,17 +1,21 @@
-import config from '../../config.json'
 import gulp from 'gulp'
 import critical from 'critical'
+import yargs from 'yargs'
+import config from '../../config.json'
 
+const argv = yargs.argv
 
 const criticalcss = () => {
+  <% if (proxyUrl != false) { %>
+    const localURL = '<%= proxyUrl %>',
+  <% } else { %>
+    const localURL = 'http://localhost:3000'
+  <% } %>
+  const url = argv.url || localURL
+
   return critical.generate({
     base: config.src.templates,
-
-    <% if (proxyUrl != false) { %>
-    src: '<%= proxyUrl %>',
-    <% } else { %>
-    src: 'http://localhost:3000'
-    <% } %>
+    src: url,
     dest: 'critical.min.css.html',
     minify: true,
     width: 1440,
