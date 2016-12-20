b2387ec87cf2b0fd66cc8d042076e72ca673dbea
David Hellmann <davidhellmann.com@gmail.com>
2016-12-20 12:47:15 +0100

    Create preloader.js

---
+++ app/templates/___src/assets/js/scripts/preloader.js
@@ -0,0 +1,37 @@
+const cfg = {
+  body: document.getElementsByTagName("body")[0],
+  states: {
+    loading: 'app_loading',
+    interact: 'app_interactive',
+    complete: 'app_complete'
+  },
+}
+
+const stateLoading = () => {
+  cfg.body.classList.add(cfg.states.loading)
+  console.log('Loading')
+}
+
+const stateInteractive = () => {
+  cfg.body.classList.remove(cfg.states.loading)
+  cfg.body.classList.add(cfg.states.interact)
+  console.log('Interact')
+}
+
+const stateComplete = () => {
+  setTimeout(() => {
+    cfg.body.classList.remove(cfg.states.interact)
+    cfg.body.classList.add(cfg.states.complete)
+    console.log('Complete')
+  }, 500)
+}
+
+const progressLoader = () => {
+  stateLoading()
+  stateInteractive()
+  document.onreadystatechange = function() {
+    "complete" === document.readyState && stateComplete()
+  }
+}
+
+progressLoader()
