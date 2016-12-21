650ba7aa22101e00dd66722b8a6cea79bd870cbf
David Hellmann <davidhellmann.com@gmail.com>
2016-12-21 20:22:15 +0100

    Update and rename addListenerMulti.js to setListener.js

--- app/templates/___src/assets/js/lib/addListenerMulti.js
+++
@@ -1,9 +0,0 @@
-//  --------------------------------------------------------
-//  Add Multiple Event Listener
-//  --------------------------------------------------------
-
-function addListenerMulti(el, s, fn) {
-  s.split(' ').forEach(e => el.addEventListener(e, fn, false))
-}
-
-export default addListenerMulti
