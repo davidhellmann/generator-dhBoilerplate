29b98e86aae1850389c144c7530913765f162a33
David Hellmann <davidhellmann.com@gmail.com>
2017-02-01 15:03:22 +0100

    Fix Preloader

--- app/templates/___src/assets/js/scripts/preloader.js
+++ app/templates/___src/assets/js/scripts/preloader.js
@@ -1,37 +1,37 @@
 const cfg = {
-  body: document.getElementsByTagName("body")[0],
-  states: {
-    loading: 'app_loading',
-    interact: 'app_interactive',
-    complete: 'app_complete'
-  },
+    body: document.getElementsByTagName("body")[0],
+    states: {
+        loading: 'is-loading',
+        interact: 'is-interactive',
+        complete: 'is-complete'
+    },
 }
 
 const stateLoading = () => {
-  cfg.body.classList.add(cfg.states.loading)
-  console.log('Loading')
+    cfg.body.classList.add(cfg.states.loading)
+    // console.log('Loading')
 }
 
 const stateInteractive = () => {
-  cfg.body.classList.remove(cfg.states.loading)
-  cfg.body.classList.add(cfg.states.interact)
-  console.log('Interact')
+    cfg.body.classList.remove(cfg.states.loading)
+    cfg.body.classList.add(cfg.states.interact)
+    // console.log('Interact')
 }
 
 const stateComplete = () => {
-  setTimeout(() => {
-    cfg.body.classList.remove(cfg.states.interact)
-    cfg.body.classList.add(cfg.states.complete)
-    console.log('Complete')
-  }, 500)
+    setTimeout(() => {
+        cfg.body.classList.remove(cfg.states.interact)
+        cfg.body.classList.add(cfg.states.complete)
+        // console.log('Complete')
+    }, 500)
 }
 
 const progressLoader = () => {
-  stateLoading()
-  stateInteractive()
-  document.onreadystatechange = function() {
-    "complete" === document.readyState && stateComplete()
-  }
+    stateLoading()
+    stateInteractive()
+    document.onreadystatechange = function() {
+        "complete" === document.readyState && stateComplete()
+    }
 }
 
 progressLoader()
