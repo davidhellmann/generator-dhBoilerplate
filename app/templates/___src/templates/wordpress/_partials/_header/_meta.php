ca17051326e124cf5ec6a3bcdc68f1e96ad0cd50
David Hellmann <davidhellmann@Davids-MacBook-Pro.local>
2017-02-06 01:39:58 +0100

    Meta Tags

--- app/templates/___src/templates/wordpress/_partials/_header/_meta.php
+++ app/templates/___src/templates/wordpress/_partials/_header/_meta.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta name="robots" content="index,follow" />
 
