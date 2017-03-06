f7e79e889e8f4c878cfd38672070e396e85cee5f
David Hellmann <davidhellmann.com@gmail.com>
2017-03-06 09:35:58 +0100

    Fix Clean Task

--- app/templates/___src/gulp/tasks/clean.js
+++ app/templates/___src/gulp/tasks/clean.js
@@ -4,42 +4,43 @@ import del from 'del';
 
 var directoryToClean;
 
-gulp.task('clean:dist', function (cb) {
-  return del([
-    config.dist.markup + '**/*'
-  ], {
-    force: true
-  }, cb);
+gulp.task('clean:dist', function(cb) {
+    return del([
+        config.dist.markup + '**/*'
+    ], {
+        force: true
+    }, cb);
 });
 
-gulp.task('clean:templates', function (cb) {
-  return del([
-    config.dist.markup + '**/*.{php,html,twig,rss}'
-  ], {
-    force: true
-  }, cb);
+gulp.task('clean:templates', function(cb) {
+    return del([
+        config.dist.markup + '**/*'
+        //config.dist.markup + '**/*.{php,html,twig,rss}'
+    ], {
+        force: true
+    }, cb);
 });
 
-gulp.task('clean:images', function (cb) {
-  return del([
-    config.dist.images.base + '**/*.{jpeg,jpg,gif,png,webp,svg,ico}'
-  ], {
-    force: true
-  }, cb);
+gulp.task('clean:images', function(cb) {
+    return del([
+        config.dist.images.base + '**/*.{jpeg,jpg,gif,png,webp,svg,ico}'
+    ], {
+        force: true
+    }, cb);
 });
 
-gulp.task('clean:js', function (cb) {
-  return del([
-    config.dist.js + '**/*'
-  ], {
-    force: true
-  }, cb);
+gulp.task('clean:js', function(cb) {
+    return del([
+        config.dist.js + '**/*'
+    ], {
+        force: true
+    }, cb);
 });
 
-gulp.task('clean:css', function (cb) {
-  return del([
-    config.dist.css + '**/*'
-  ], {
-    force: true
-  }, cb);
+gulp.task('clean:css', function(cb) {
+    return del([
+        config.dist.css + '**/*'
+    ], {
+        force: true
+    }, cb);
 });
