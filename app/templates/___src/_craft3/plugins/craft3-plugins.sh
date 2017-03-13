be3525e42664c2daadf7a27a578a7383e2a94f29
David Hellmann <davidhellmann.com@gmail.com>
2017-03-13 14:34:26 +0100

    Update Composer Json

--- app/templates/___src/_craft3/plugins/craft3-plugins.sh
+++ app/templates/___src/_craft3/plugins/craft3-plugins.sh
@@ -17,14 +17,39 @@ echo
 echo
 echo
 
-# https://github.com/nystudio107/craft3-minify
-composer require nystudio107/craft3-minify
 
 # https://github.com/nystudio107/craft3-cookies
 composer require nystudio107/craft3-cookies
+
+# https://github.com/nystudio107/craft3-minify
+composer require nystudio107/craft3-minify
+
+# https://github.com/nystudio107/craft3-pathtools
+composer require nystudio107/craft3-pathtools
+
+# https://github.com/nystudio107/craft3-richvariables
+# composer require nystudio107/craft3-richvariables
 
 # https://github.com/craftcms/mailgun
 composer require craftcms/mailgun
 
 # https://github.com/TopShelfCraft/Craft3-Walk
 composer require topshelfcraft/craft3-walk
+
+# https://github.com/selvinortiz/doxter
+# composer require selvinortiz/doxter
+
+# https://github.com/craftcms/element-api
+# composer require craftcms/element-api
+
+# https://github.com/marionnewlevant/craft3-match_input
+composer require marionnewlevant/match-input
+
+# https://github.com/selvinortiz/patrol
+# composer require selvinortiz/patrol
+
+# https://github.com/barrelstrength/sproutnotes
+# composer require barrelstrength/sproutnotes
+
+# https://github.com/marionnewlevant/craft3-twig_perversion
+composer require marionnewlevant/twig-perversion
